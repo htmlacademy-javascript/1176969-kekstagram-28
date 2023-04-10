dc1da8d26db84f3edf55b4bfa23696022501bd41
vova-learn <vpmail13@gmail.com>
2023-04-10 13:04:47 +0300

    Добавит сортировку при переключении вкладок

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ import './pictures.js';
 import './functions.js';
 import './from.js';
 import { getData } from './api.js';
-// import { renderPictures } from './pictures.js';
 import { errorLoadDataElement } from './alerts.js';
 import { initTabs } from './tabs.js';
 
 
 getData(errorLoadDataElement)
   .then((pictures) => initTabs(pictures));
-// .then((pictures) => renderPictures(pictures));
--- js/tabs.js
+++ js/tabs.js
@@ -1,8 +1,55 @@
 import { renderPictures } from './pictures.js';
 
+const filterShema = {
+  'По умолчанию': 'default',
+  'Случайные': 'random',
+  'Обсуждаемые': 'pop',
+};
+
+const updateActiveTabClass = (tabsMenuElement, tabButtonElement) => {
+  tabsMenuElement
+    .querySelector('.img-filters__button.img-filters__button--active')
+    .classList
+    .remove('img-filters__button--active');
+  tabButtonElement.classList.add('img-filters__button--active');
+};
+
+const removePictures = () => {
+  const picturesElements = document.querySelectorAll('.pictures .picture');
+
+  for (const picture of picturesElements) {
+    picture.remove();
+  }
+};
+
+const getFilterPictures = (filter, pictures) => {
+  switch(filter) {
+    case 'random':
+      return pictures.slice().sort(() => Math.random() - 0.5).slice(0,10);
+    case 'pop':
+      return pictures.slice().sort((a,b) => b.comments.length - a.comments.length);
+    default:
+      return pictures;
+  }
+};
+
 export const initTabs = (pictures) => {
   const tabsMenuElement = document.querySelector('.img-filters');
   tabsMenuElement.classList.remove('img-filters--inactive');
 
   renderPictures(pictures);
+
+  tabsMenuElement.addEventListener('click', ({target}) => {
+    const tabButtonElement = target.closest('.img-filters__button:not(.img-filters__button--active)');
+
+    if (!tabButtonElement) {
+      return;
+    }
+
+    const filter = filterShema[tabButtonElement.textContent];
+    const filterPictures = getFilterPictures(filter, pictures);
+    updateActiveTabClass(tabsMenuElement, tabButtonElement);
+    removePictures();
+    renderPictures(filterPictures);
+  });
 };
