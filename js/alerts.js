9e2895da15661a9a31482b85ff456489c3712a6c
vova-learn <vpmail13@gmail.com>
2023-04-11 07:53:21 +0300

    Добавит варианты закрытия окна успешной отправки

--- js/alerts.js
+++ js/alerts.js
@@ -26,12 +26,24 @@ export const loadingElement = {
 export const successElement = {
   mainElement: document.querySelector('main'),
   element: document.querySelector('#success').content.querySelector('.success').cloneNode(true),
+  handleEscKeydown({key}) {
+    if (key === 'Escape') {
+      successElement.remove();
+    }
+  },
   render() {
     this.element.querySelector('.success__button').onclick = () => this.remove();
+    this.element.addEventListener('click', ({target}) => {
+      if (!target.closest('.success__inner')) {
+        this.remove();
+      }
+    });
+    document.body.addEventListener('keydown', this.handleEscKeydown);
     this.mainElement.appendChild(this.element);
   },
   remove() {
-    this.element.remove();
+    document.body.removeEventListener('keydown', this.handleEscKeydown);
+    successElement.element.remove();
   },
 };
 
